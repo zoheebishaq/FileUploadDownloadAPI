e461405dec531c9e9492a2ba7ea9928a84de5387
Zoheeb Ishaque <63728270+zoheebishaq@users.noreply.github.com>
2022-03-03 20:00:35 +0100

    Initial commit

---
+++ src/main/java/com/example/fileuploaddownloadapi/FileUploadDownloadApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.fileuploaddownloadapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadDownloadApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileUploadDownloadApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/fileuploaddownloadapi/download/service/FileDownloadUtil.java
@@ -0,0 +1,31 @@
+package com.example.fileuploaddownloadapi.download.service;
+
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileDownloadUtil {
+    private Path foundFile;
+    public Resource getFileAsResource(String fileCode) throws IOException {
+        Path uploadDirectory = Paths.get("Files-Upload");
+
+        Files.list(uploadDirectory).forEach(file ->{
+            if (file.getFileName().toString().startsWith(fileCode)){
+                foundFile =file;
+                return;
+            }
+        });
+
+        if (foundFile != null){
+            return new UrlResource(foundFile.toUri());
+        }
+
+        return null;
+    }
+
+}
